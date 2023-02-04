25e643aa2505ad2c108b32f27ea9ab00cd063110
Christina Woodhams <cwoodhams89@gmail.com>
2023-02-04 16:58:30 -0500

    First commit. Unit3 step 1.

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import './Header.css';
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  return (
+    <header>
+      <h2>Devmountain Eatery</h2>
+      <nav>
+        <Link to=''>
+          <button className="header-btn btn-hov">Home</button>
+        </Link>
+        <Link to='/newRecipe'>
+          <button className="header-btn btn-hov">Add Recipe</button>
+        </Link>  
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
